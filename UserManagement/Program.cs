2b36e506a912247beb69faa0eaa9898c887ca241
Akmal <akmalnomad1501@gmail.com>
2026-04-14 21:58:33 +0500

    Fix migration

--- UserManagement/Program.cs
+++ UserManagement/Program.cs
@@ -6,14 +6,12 @@ using UserManagement.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Database
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
     ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 
-// Identity - minimal password requirements per task spec
 builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
 {
     options.Password.RequireDigit = false;
@@ -29,7 +27,6 @@ builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
 .AddEntityFrameworkStores<ApplicationDbContext>()
 .AddDefaultTokenProviders();
 
-// Cookie config
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = "/Account/Login";
@@ -43,11 +40,10 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Auto-migrate on startup
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.Migrate();
+    db.Database.EnsureCreated();
 }
 
 if (!app.Environment.IsDevelopment())
